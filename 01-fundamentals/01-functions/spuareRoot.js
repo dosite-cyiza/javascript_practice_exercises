ff7dd19aac0f665411ef7981b66d2b466185317a
“[Nkusi12]” <[dositecyiza@gmail.com]>
2025-02-04 20:53:33 +0200

    Square of numbers by using fumction

---
+++ 01-fundamentals/01-functions/spuareRoot.js
@@ -0,0 +1,10 @@
+/*
+Write a function that returns the square of a number
+*/
+let num = 2
+const spuareRootOfNumber = (num) =>{
+
+    return  num * num
+    
+}
+console.log(spuareRootOfNumber(num));
