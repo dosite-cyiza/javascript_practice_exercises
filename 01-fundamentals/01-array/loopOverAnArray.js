dd3c5ffa0e95c24ec5879dae2e23dafe89bb8911
“[Nkusi12]” <[dositecyiza@gmail.com]>
2025-02-06 17:22:01 +0200

    loop over an arry.

---
+++ 01-fundamentals/01-array/loopOverAnArray.js
@@ -0,0 +1,11 @@
+/*
+ Loop Over an Array
+*/
+const names =['doe','john','henry','hook']
+console.log(names.forEach(name =>{
+    // add element on array by using cocant()
+    console.log( names.concat('s' ))
+    console.log(name + 's')
+    
+}));
+console.log("me");
