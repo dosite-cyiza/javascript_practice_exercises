d37f51635bd7ad8dd417a72002f50956aa5ae3e5
“[Nkusi12]” <[dositecyiza@gmail.com]>
2025-02-11 20:47:13 +0200

    find Integral

---
+++ 01-fundamentals/01-functions/ConvertStringToUpperCase.js
@@ -0,0 +1,7 @@
+/*Write a function which converts the input string to uppercase. */
+const convertToUpperCase =() =>{
+
+    const inputString =window.prompt("Enter a string:")
+    inputString.toUpperCase()
+}
+convertToUpperCase()
