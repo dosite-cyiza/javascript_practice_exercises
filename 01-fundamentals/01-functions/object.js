d2122406319c0f872307571ab7f598743ba77679
“[Nkusi12]” <[dositecyiza@gmail.com]>
2025-02-24 10:21:20 +0200

    about how to access values of objects

---
+++ 01-fundamentals/01-functions/object.js
@@ -0,0 +1,9 @@
+let person = { name: "Alice", age: 25, country: "Rwanda" }
+
+for (let key in person) {
+    // console.log(Object.keys(person));
+    // console.log(Object.values(person));
+    // console.log(Object.entries(person));
+    console.log(key,":", person[key]);
+
+}
