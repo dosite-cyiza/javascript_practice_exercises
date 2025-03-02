c3ff5e62595f35501edc6236bdbe5915917b8bad
“[Nkusi12]” <[dositecyiza@gmail.com]>
2025-03-02 20:45:05 +0200

    Counting sheep in the array that don't have null,undefined values meanas that's present

--- 01-fundamentals/01-functions/removeDuplicateFromArray.js
+++ 01-fundamentals/01-functions/removeDuplicateFromArray.js
@@ -4,6 +4,7 @@ Remove the duplicate from array without using Set constructor
 */
 
 const removeDuplicate = (arr) =>{
-    return arr.filter((element,index) => arr.indexOf(element) === index)
+    // return arr.filter((element,index) => arr.indexOf(element) === index)
+    return [...new Set(arr)]
 }
 console.log(removeDuplicate([1,1,2,3,4,5]))
