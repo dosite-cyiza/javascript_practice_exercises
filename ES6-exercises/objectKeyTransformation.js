14b9f278cf64327aa88eb5e02187225a3d09d89e
dosite-cyiza <dositecyiza@gmail.com>
2025-08-11 19:25:18 +0200

    Object Transformation

---
+++ ES6-exercises/objectKeyTransformation.js
@@ -0,0 +1,44 @@
+/**
+ Object key transformation
+
+Your task is to implement a JavaScript function called transformKeys that 
+takes an object as input and returns a new object with the keys transformed according 
+to the provided transformation function.
+
+The transformKeys function should iterate through the keys of the input 
+object and apply the transformation function to each key. The transformation
+ function should take the original key as input and return the new key.
+
+Example:
+```
+const inputObject = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+};
+
+const transformFunction = (key) => key.toUpperCase();
+const transformedObject = transformKeys(inputObject, transformFunction);
+console.log(transformedObject);
+// should return { FIRSTNAME: "John", LASTNAME: "Doe", AGE: 30 }
+```
+ */
+let inputObj =   {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30
+}
+const transformKeys = (obj) => {
+
+    let result
+    for(key in obj){
+      result = transformationFunction(key)
+    }
+    return result
+
+};
+const transformationFunction = (key)=>{
+return key.toUpperCase()
+}
+
+console.log(transformKeys(inputObj,transformationFunction));
