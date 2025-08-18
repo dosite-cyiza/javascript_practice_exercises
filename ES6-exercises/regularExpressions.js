3352b36052ff6c01618d616dbc6af253d9d5ba7f
dosite-cyiza <dositecyiza@gmail.com>
2025-08-18 12:28:13 +0200

    Use Test method

---
+++ ES6-exercises/regularExpressions.js
@@ -0,0 +1,12 @@
+/**
+ All methods related to regular Expressions
+ */
+
+ //---------------------------- Test Method (This method takes regex to string and return true or false)------------------
+ console.log("Test Method")
+ const string = "Hello World"
+ const myRegex = /Hello/
+ const result = myRegex.test(string)
+ console.log(result)
+
+ 
