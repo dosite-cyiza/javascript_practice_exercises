36c43a22fdb6f89031a6aebf97f9857a3e8d02ac
dosite-cyiza <dositecyiza@gmail.com>
2025-09-22 08:15:22 +0200

    Greatest Common divisor

--- ES6-exercises/greatestDivisor.js
+++ ES6-exercises/greatestDivisor.js
@@ -4,3 +4,11 @@ How would you find the greatest common divisor of two numbers?
 
  */
 
+const greatestCommonDivisor = (a,b) =>{
+    if(b === 0){
+        return a
+    }
+    return greatestCommonDivisor(b,a % b)
+
+}
+console.log(greatestCommonDivisor(3,5))
