388979722c80479f60916f1cadcda70aa4f6492f
dosite-cyiza <dositecyiza@gmail.com>
2025-09-23 20:09:35 +0200

    new answers

---
+++ convertNumberIntoArrayOfDigits.js
@@ -0,0 +1,12 @@
+/*
+Write a JavaScript program to convert a specified number into an array of digits.   
+ "123355" => [1,2,3,3,5,5] 123355 => [1,2,3,3,5,5]
+*/
+//first way 
+const convertNumber =(num) => num.toString().split("").map(Number) 
+console.log(convertNumber(123456))
+
+// Second Way
+
+const convertNumbers =(num) => [...num.toString()].map(Number)
+console.log(convertNumbers(1278787798))
