0ed874ed8f1aea5f934c5e26304339a22288425f
dosite-cyiza <dositecyiza@gmail.com>
2025-09-23 20:32:34 +0200

    counting zeros

--- countingZeros.js
+++ countingZeros.js
@@ -10,35 +10,19 @@ Explanation: So the trick here is. if you have a number 1 to 50 the value
  */
 
 
-// const countTotalZeros = (n) =>{
-//   let totalZeros = 0;
-//   for (let i = 1; i <= n; i++) {
-//     let numStr = i.toString();
-//     for (let j = 0; j < numStr.length; j++) {
-//       if (numStr[j] === '0') {
-//         totalZeros++;
-//       }
-//     }
-//   }
-//   return totalZeros;
-// }
-
-// console.log(countTotalZeros(50)); 
-// console.log(countTotalZeros(100)); 
-// console.log(countTotalZeros(200));  
-
-const countingZeros = (n) =>{
-  let numStr
-  let count = 0
-  for( let i = 0; i <= n; i++){
-    numStr = i.toString()
- for( let j = 0; numStr.length; j++){
-      if (numStr[j] == 0){
-        count ++
+const countTotalZeros = (n) =>{
+  let totalZeros = 0;
+  for (let i = 1; i <= n; i++) {
+    let numStr = i.toString();
+    for (let j = 0; j < numStr.length; j++) {
+      if (numStr[j] === '0') {
+        totalZeros++;
       }
- }
-   return count 
+    }
   }
-
+  return totalZeros;
 }
-console.log(countingZeros(50))
+
+console.log(countTotalZeros(50)); 
+console.log(countTotalZeros(100)); 
+console.log(countTotalZeros(200));  
